a414fafbd0c3c52126f9138d3d041cb6f676d0e0
Clockshop Dev <erbnando+clockshop@gmail.com>
2017-09-16 16:16:32 -0700

    updates to homepage styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,11 @@
 		function search() {
 			$('.search').on('click', function() {
 				if ($('#searchform').is(':visible')) {
-					$('#searchform').fadeOut();
+					$('#searchform').hide();
+					$('.menu-primary-navigation-container').css('opacity', '1');
 					$('.search .fa-times').replaceWith( "<i class='fa fa-search' aria-hidden='true'></i>" );
 				} else {
+					$('.menu-primary-navigation-container').css('opacity', '0');
 					$('#searchform').fadeIn();
 					$('.search .fa-search').replaceWith( "<i class='fa fa-times' aria-hidden='true'></i>" );
 				}
