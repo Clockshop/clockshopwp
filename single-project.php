8bf95d42b0e496fcb2fae97edac336839bab84b4
Nando Ramirez <erbnando@gmail.com>
2017-12-11 23:27:49 -0500

    work on project page events layout

--- single-project.php
+++ single-project.php
@@ -22,7 +22,34 @@ $events = tribe_get_events( array(
 	),
 ) );
 
+$past_events = tribe_get_events( array(
+	'end_date' => date("Y-m-d"),
+	'posts_per_page'   => -1,
+	'order' => 'DESC',
+	'tax_query' => array(
+		array(
+			'taxonomy' => 'projects',
+			'field'    => 'slug',
+			'terms'    => $term[0]->slug,
+		),
+	),
+));
+
+$eventYears = array();
+$years = array();
+
+foreach($past_events as $key => $event) {
+	$eventDate = $event->EventStartDate;
+	$eventYear = date('Y', strtotime($eventDate));
+	if (!in_array($eventYear, $years)) {
+		array_push($years, $eventYear);
+		$eventYears[$eventYear]['year'] = $eventYear;
+	}
+	$eventYears[$eventYear]['events'][$key] = $event;
+}
+
 $context['events'] = $events;
+$context['eventYears'] = $eventYears;
 
 Timber::render( 'views/project.twig', $context );
 
