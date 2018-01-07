83cc79650babdeefc2dd4048ae8175fc7d28d08b
Clockshop Dev <erbnando+clockshop@gmail.com>
2018-01-06 23:07:20 -0500

    cleaned up theme

---
+++ assets/js/all.js
@@ -0,0 +1,411 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	__webpack_require__(1);
+	__webpack_require__(2);
+	__webpack_require__(3);
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+	// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports) {
+
+	(function($) {
+
+		$(document).ready(function() {
+			
+			function search() {
+				$('.search').on('click', function() {
+					if ($('.topnav .searchform').is(':visible')) {
+						$('.topnav .searchform').hide();
+						$('.menu-container').css('opacity', '1');
+						$('.search .fa-times').replaceWith( "<i class='fa fa-search' aria-hidden='true'></i>" );
+						$('.topnav .searchform input').focus();
+					} else {
+						$('.menu-container').css('opacity', '0');
+						$('.topnav .searchform').fadeIn();
+						$('.search .fa-search').replaceWith( "<i class='fa fa-times' aria-hidden='true'></i>" );
+					}
+				});
+			}	
+
+			function collapsable_sections() {
+				$('.collapsable-sections section .fa').on('click', function() {
+					if ($(this).hasClass('fa-plus')) {
+						$(this).removeClass('fa-plus');
+						$(this).addClass('fa-minus');
+						$(this).next().show();
+					} else {
+						$(this).removeClass('fa-minus');
+						$(this).addClass('fa-plus');
+						$(this).next().hide();
+					}
+				});
+			}
+
+			function eventsSlider() {
+				$('.events-slider').slick({
+					arrows: false,
+					adaptiveHeight: true,
+					autoplay: true,
+					autoplaySpeed: 10000,
+				});
+
+				$('.prev').on( 'click', function() {
+					$('.events-slider').slick('slickPrev');
+				});
+
+				$('.next').on( 'click', function() {
+					$('.events-slider').slick('slickNext');
+				});
+			}
+
+			function eventImages() {
+				$('.event-images').slick({
+					arrows: false,
+					adaptiveHeight: true,
+					autoplay: true,
+					autoplaySpeed: 10000,
+				});
+
+				$('.prev').on( 'click', function() {
+					$('.event-images').slick('slickPrev');
+				});
+
+				$('.next').on( 'click', function() {
+					$('.event-images').slick('slickNext');
+				});
+			}
+
+			function resizeHeader() {
+				$(window).scroll(function(){
+					if ($(document).scrollTop() > 0) {
+						if ($('.primarynav').hasClass('top')) $('.primarynav').removeClass('top');
+						if ($('.layoutspacer').hasClass('top')) $('.layoutspacer').removeClass('top');
+						$('.primarynav').addClass('scroll');
+						$('.layoutspacer').addClass('scroll');
+					} else {
+						if ($('.primarynav').hasClass('scroll')) $('.primarynav').removeClass('scroll');
+						if ($('.layoutspacer').hasClass('scroll')) $('.layoutspacer').removeClass('scroll');
+						$('.primarynav').addClass('top');
+						$('.layoutspacer').addClass('top');
+					}
+				});
+			}
+
+			function hideBrokenImages() {
+				$(".search-template article img").load(function() { 
+				    $(this).css('opacity', '1');
+				});
+			}
+
+			function projectSlider() {
+				$('.project-slider').slick({
+					autoplay: true,
+					autoplaySpeed: 7500,
+					adaptiveHeight: true,
+				});
+			}
+
+			function removeEmptyPTags() {
+				$('p:empty').remove();
+			}
+
+			collapsable_sections();
+			search();
+			eventsSlider();
+			eventImages();
+			resizeHeader();
+			hideBrokenImages();
+			projectSlider();
+			removeEmptyPTags();
+		});
+
+		function signUpForm() {
+		    $(document).bind('gform_post_render', function(event, formId, currentPage){
+		    	if(formId == 4) {
+		    		$('#input_4_1').attr("placeholder", "Your Email Address...");
+					$(".gform_validation_error .ginput_container_email input").addClass('error');
+					$(".gform_validation_error .ginput_container_email input").keydown(function() {
+						$(this).removeClass('error');
+					});
+		    	}
+		    });
+		}
+
+		signUpForm();
+
+	})(jQuery);
+
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);	  
+
+	var players = document.getElementsByClassName("youtube-video");
+
+	function onYouTubeIframeAPIReady() {
+		player = [];
+		for (var i = 0; i < players.length; i++) {
+	        player[players[i].id] = new YT.Player(players[i].id, {
+				videoId: players[i].id,
+				events: {
+					'onReady': onPlayerReady,
+					//'onStateChange': onPlayerStateChange
+					playerVars: { controls: 0, modestbranding: 1, rel: 0 },
+				}
+			});
+		}
+	}
+
+	function onPlayerReady() {
+		for (var i = 0; i < players.length; i++) {
+			//console.log(document.getElementById('video-container-' + players[i].id));
+			if (document.getElementById('video-container-' + players[i].id)) {
+				document.getElementById('video-container-' + players[i].id).style.opacity = "1";
+			}
+		}
+	}
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports) {
+
+	(function($) {
+
+		var topLinkVisible = false;
+		var topNavVisible = false;
+		var mobileNav = false;
+		var scrollTimer = null;
+
+		function isMobile() {
+			if ( $(".responsivecue").css("float") == "right" ) { 
+				return false;
+			} else {
+				return true;
+			}
+		}
+
+		function openNav() {
+			if ( topNavVisible == false ) {
+
+				$('.opennav').hide();
+				$('.closenav').show();
+
+				$( ".topnav" ).show();
+				topNavVisible = true;
+			}
+
+		}
+
+		function closeNav() {
+
+			if ( topNavVisible == true ) {
+
+				$('.closenav').hide();
+				$('.opennav').show();
+
+				$( ".topnav" ).hide();
+				topNavVisible = false;
+
+			}
+
+		}
+
+
+		function handleScroll() {
+
+			var scrolltop = $(window).scrollTop();
+
+			if ( isMobile() ) {
+
+				hideToplink();
+
+			} else { 
+
+				if ( scrolltop > 1000 ) {
+					showToplink();
+				} else {
+					hideToplink();
+				}
+			}
+
+		}
+
+
+
+		function showToplink() {
+			if ( topLinkVisible == false ) {
+				topLinkVisible = true;
+				$("#uparrow").stop().fadeIn("fast");
+			}
+		}
+
+		function hideToplink() {
+			if ( topLinkVisible == true ) {
+				topLinkVisible = false;
+				$("#uparrow").stop().fadeOut("fast");
+			}
+		}
+
+
+		function setupLayout() {
+
+			$('body').addClass('js');
+
+			handleResize();
+
+			$("#uparrow").hide();
+
+		}
+
+		function handleResize() {
+			if ( isMobile() ) {
+				if ( mobileNav == false ) {
+					// Transition to mobile
+
+					$('.topnav').hide();
+					$('.closenav').hide();
+					$('.opennav').show();
+
+					topNavVisible = false;
+					mobileNav = true;
+
+				}
+
+			} else {
+				if ( mobileNav == true ) {
+					//$('.topnav').show();
+					mobileNav = false;
+				}
+
+			}
+
+			setupPadding();
+		}
+
+		function setupPadding() {
+
+			var adminbarheight = 0;
+
+			if ($('#wpadminbar').length != 0) {
+				var adminbarheight = $('#wpadminbar').outerHeight();
+			}
+
+			$('#uparrow').css('top', adminbarheight + 'px');
+			$('.wpadminbarspacer').css({'height': adminbarheight + 'px'});
+
+
+			var topTitle = 0;
+
+			if ( isMobile() ) { 
+
+				topTitle += $('.mobiletoggle').outerHeight();
+
+			} else { 
+				topTitle += $('.topnav').outerHeight();
+			}
+
+			$('.layoutspacer').css({'height': topTitle + 'px'});
+
+		}
+
+		$(document).ready( function() {
+
+			setupLayout();
+
+			$('#uparrow').on( "click", function(event) {
+				event.preventDefault();
+				$('body,html').animate({
+					scrollTop: 0
+				}, 200);
+				return false;
+			});
+
+			$('.opennav').on('click', function( event ) {
+				openNav();
+			});
+
+			$('.closenav').on('click', function( event ) {
+				closeNav();
+			});
+
+		});
+
+
+		$(window).load(function() {
+
+			$('.flexslider').flexslider({
+			    controlNav: false,
+			    directionNav: false
+			});
+
+		});
+
+
+		$(window).scroll(function() {
+
+		    if (scrollTimer) {
+		        clearTimeout(scrollTimer);   // clear any previous pending timer
+		    }
+		    scrollTimer = setTimeout(handleScroll, 1);   // set new timer
+
+		});
+
+		$(window).resize(function(){
+			handleResize();
+		});
+
+	})(jQuery);
+
+
+/***/ })
+/******/ ]);
--- lib/enqueue.php
+++ lib/enqueue.php
@@ -4,9 +4,6 @@ function enqueue_scripts_method() {
 
 	$version = "h";
 
-	// Remove Unnecessary Code
-	// http://www.themelab.com/2010/07/11/remove-code-wordpress-header/
-
 	remove_action('wp_head', 'rsd_link');
 	remove_action('wp_head', 'wlwmanifest_link');
 	remove_action('wp_head', 'wp_generator');
@@ -16,91 +13,55 @@ function enqueue_scripts_method() {
 
 	if(!wp_script_is('jquery')) wp_enqueue_script("jquery");
 
-	// Define JS
-
-	$flexsliderjs = get_template_directory_uri() . '/js/jquery.flexslider.js';
-	wp_register_script('flexsliderjs',$flexsliderjs, false, $version);
-
-	$mainjs = get_template_directory_uri() . '/js/main.js';
-	wp_register_script('mainjs',$mainjs, false, $version);
-
-	$scripts = get_template_directory_uri() . '/js/scripts.js';
-	wp_register_script('scripts', $scripts, false, $version);
-
-	$projecttemplatejs = get_template_directory_uri() . '/js/projecttemplate.js';
-	wp_register_script('projecttemplatejs',$projecttemplatejs, false, $version);
-
-	$flickityjs = get_template_directory_uri() . '/js/flickity.pkgd.min.js';
-	wp_register_script('flickityjs',$flickityjs, false, $version);
-
-	$descriptivehomejs = get_template_directory_uri() . '/js/descriptivehome.js';
+	// js
+	$alljs = get_template_directory_uri() . '/assets/js/all.js';
+	wp_register_script('alljs', $alljs, false);
+	$slickjs = get_template_directory_uri() . '/assets/js/slick.min.js';
+	wp_register_script('slickjs', $slickjs, false);
+	$flexsliderjs = get_template_directory_uri() . '/assets/js/jquery.flexslider.js';
+	wp_register_script('flexsliderjs',$flexsliderjs, false);
+	$flickityjs = get_template_directory_uri() . '/assets/js/flickity.pkgd.min.js';
+	wp_register_script('flickityjs',$flickityjs, false);
+	$descriptivehomejs = get_template_directory_uri() . '/assets/js/descriptivehome.js';
 	wp_register_script('descriptivehomejs', $descriptivehomejs, false, $version);
-
-	$slickjs = get_template_directory_uri() . '/js/slick.min.js';
-	wp_register_script('slickjs', $slickjs, false, $version);
-
 	$viemoPlayer = 'https://player.vimeo.com/api/player.js';
 	wp_register_script('vimeoPlayer', $viemoPlayer, false);
 	
-	// Define CSS
-
-	$ralewaycss = 'http://fonts.googleapis.com/css?family=Raleway:400,300,200,100';
-	wp_register_style('ralewaycss',$ralewaycss, false, $version);
-
-	$ptsanscss = 'http://fonts.googleapis.com/css?family=PT+Sans+Narrow';
-	wp_register_style('ptsanscss',$ptsanscss, false, $version);
-
-	$flexslidercss = get_stylesheet_directory_uri() . '/css/flexslider.css';
-	wp_register_style('flexslidercss',$flexslidercss, false, $version);
-
-	$normalizecss = get_stylesheet_directory_uri() . '/css/normalize.css';
-	wp_register_style('normalizecss',$normalizecss, false, $version);
-
-	$themecss = get_stylesheet_directory_uri() . '/style.css';
-	wp_register_style('themecss',$themecss, false, $version);
-
-	$flickitycss = get_stylesheet_directory_uri() . '/css/flickity.min.css';
-	wp_register_style('flickitycss',$flickitycss, false, $version);
-
-	$descriptivehomecss = get_stylesheet_directory_uri() . '/css/descriptivehome.css';
-	wp_register_style('descriptivehomecss',$descriptivehomecss, false, $version);
-
+	// css
+	$style = get_stylesheet_directory_uri() . '/assets/css/style.css';
+	wp_register_style('style', $style, false);
+	$allcss = get_stylesheet_directory_uri() . '/assets/css/all.css';
+	wp_register_style('allcss', $allcss, false);
+	$normalizecss = get_stylesheet_directory_uri() . '/assets/css/normalize.css';
+	wp_register_style('normalizecss',$normalizecss, false);
+	$descriptivehomecss = get_stylesheet_directory_uri() . '/assets/css/descriptivehome.css';
+	wp_register_style('descriptivehomecss',$descriptivehomecss, false);
 	$fontawesome = get_stylesheet_directory_uri() . '/assets/fonts/font-awesome.min.css';
-	wp_register_style('fontawesome',$fontawesome, false, $version);
+	wp_register_style('fontawesome',$fontawesome, false);
+	$slickcss = get_template_directory_uri() . '/assets/css/slick.css';
+	wp_register_style('slickcss', $slickcss, false);
+	$flexslidercss = get_stylesheet_directory_uri() . '/assets/css/flexslider.css';
+	wp_register_style('flexslidercss', $flexslidercss, false);
+	$flickitycss = get_stylesheet_directory_uri() . '/assets/css/flickity.min.css';
+	wp_register_style('flickitycss', $flickitycss, false);
 
-	$all = get_stylesheet_directory_uri() . '/assets/css/all.css';
-	wp_register_style('all', $all, false, $version);
-
-	$slickcss = get_template_directory_uri() . '/js/slick.css';
-	wp_register_style('slickcss', $slickcss, false, $version);
-
-	// Enqueue
-
-	wp_enqueue_script( 'flexsliderjs',array('jquery'));
-	wp_enqueue_script( 'flickityjs',array('jquery'));
-	wp_enqueue_script( 'slickjs',array('jquery'));
-	wp_enqueue_script( 'mainjs',array('jquery','flexsliderjs'));
-	wp_enqueue_script( 'scripts',array('jquery'));
-	wp_enqueue_script( 'projecttemplatejs',array('jquery'));
+	// enqueue js
+	wp_enqueue_script( 'alljs', array('jquery'));
+	wp_enqueue_script( 'slickjs', array('jquery'));
+	wp_enqueue_script( 'flexsliderjs', array('jquery'));
+	wp_enqueue_script( 'flickityjs', array('jquery'));
+	wp_enqueue_script( 'descriptivehomejs', array('jquery','flickityjs'));
 	wp_enqueue_script( 'vimeoPlayer' );
 
-	wp_enqueue_style( 'flickitycss');
-	wp_enqueue_style( 'flexslidercss');
-
-	wp_enqueue_style( 'ralewaycss');
-	wp_enqueue_style( 'ptsanscss');
+	// enqueue css
+	wp_enqueue_style( 'style');
+	wp_enqueue_style( 'allcss');
 	wp_enqueue_style( 'normalizecss');
-	wp_enqueue_style( 'themecss');
-
+	wp_enqueue_style( 'descriptivehomecss');
 	wp_enqueue_style( 'fontawesome');
-	wp_enqueue_style( 'all');
 	wp_enqueue_style( 'slickcss');
-
-	if ( is_page_template('page-home.php') ) :
-		wp_enqueue_style( 'descriptivehomecss');
-		wp_enqueue_script( 'descriptivehomejs',array('jquery','flickityjs'));
-	endif;
-
+	wp_enqueue_style( 'flexslidercss');
+	wp_enqueue_style( 'flickitycss');
 }
 add_action('wp_enqueue_scripts', 'enqueue_scripts_method');
 
