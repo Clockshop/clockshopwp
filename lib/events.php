a005ae1b617938a522c2ffc19889be5573424599
Clockshop Dev <erbnando+clockshop@gmail.com>
2018-01-20 18:14:20 -0500

    added fix for events single date display

--- lib/events.php
+++ lib/events.php
@@ -64,9 +64,9 @@ add_filter('pre_get_posts', 'order_events_admin');
 function order_events_admin($query) {
 	if($query->is_admin) {
 		if ($query->get('post_type') == 'events') {
-            $query->set('meta_key', '_EventStartDate' );
-            $query->set('orderby', 'meta_value_num');
-            $query->set('order', 'DESC' );
+			$query->set('meta_key', '_EventStartDate' );
+			$query->set('orderby', 'meta_value_num');
+			$query->set('order', 'DESC' );
 		}
 	}
 	return $query;
@@ -103,71 +103,49 @@ function create_venue_tax() {
 			'meta_box_cb' => false,
 			)
 		);
-	}
-	add_action( 'init', 'create_venue_tax' );
-	
-	// Format Dates
+}
+add_action( 'init', 'create_venue_tax' );
+
+// Format Dates
+function event_dates() {
 	function spellerberg_sp_date($postid, $format='') {
 		echo spellerberg_return_sp_date($postid, $format);
 	}
-	
 	function spellerberg_return_sp_date($postid, $format='') {
-		
 		$startYear = date('Y', strtotime(get_field('_EventStartDate' , $postid)));
 		$endYear = date('Y', strtotime(get_field('_EventEndDate' , $postid)));
-		
 		$startMonth = date('F', strtotime(get_field('_EventStartDate' , $postid)));
 		$endMonth = date('F', strtotime(get_field('_EventEndDate' , $postid)));
-		
 		$startDay = date('j', strtotime(get_field('_EventStartDate' , $postid)));
 		$endDay = date('j', strtotime(get_field('_EventEndDate' , $postid)));
-		
 		$weekday = date('D', strtotime(get_field('_EventStartDate' , $postid)));
-		
 		$startTime = date('g', strtotime(get_field('_EventStartDate' , $postid)));
 		$endTime = date('g', strtotime(get_field('_EventEndDate' , $postid)));
-		
 		$s_hour = date('g', strtotime(get_field('_EventStartDate' , $postid)));
 		$s_min = date('i', strtotime(get_field('_EventStartDate' , $postid)));
 		$s_am = ' ' . date('a', strtotime(get_field('_EventEndDate' , $postid)));
-		
 		$e_hour = date('g', strtotime(get_field('_EventStartDate' , $postid)));
 		$e_min = date('i', strtotime(get_field('_EventStartDate' , $postid)));
 		$e_am = ' ' . date('a', strtotime(get_field('_EventEndDate' , $postid)));
-		
-		return spellerberg_show_event_datetime( $startYear, $endYear, $startMonth, $endMonth, $startDay, $endDay, $startTime, $endTime, $weekday, $s_hour, $s_min, $s_am, $e_hour, $e_min, $e_am, $format );
-		
+		return spellerberg_show_event_datetime( $startYear, $endYear, $startMonth, $endMonth, $startDay, $endDay, $startTime, $endTime, $weekday, $s_hour, $s_min, $s_am, $e_hour, $e_min, $e_am, $format );	
 	}
-	
 	function spellerberg_show_event_datetime ( $startYear, $endYear, $startMonth, $endMonth, $startDay, $endDay, $startTime, $endTime, $weekday, $s_hour, $s_min, $s_am, $e_hour, $e_min, $e_am, $format) {
-		
 		$output = '';
-		
-		
 		if ( $startYear == $endYear && $startMonth == "Jan" && $endMonth == "Dec" && $startDay == "1" && $endDay == "31") {
-			
 			// Is a year-long program
 			$output .= 'Annual program';
-			
 		} else {
-			
 			if ( $startYear !== $endYear ) {
-				
 				//Different Years
 				$output .= $startMonth . ' ' . $startDay;
 				if ( $format != 'noyears' ) $output .= ', ' . $startYear;
 				$output .= ' &ndash; ' . $endMonth . ' ' . $endDay . ', ' . $endYear;
-				
 			} else {
-				
 				//Same Years
 				if ($startMonth !== $endMonth) {
-					
 					// Different Months
 					$output .= $startMonth . ' ' . $startDay . ' &ndash; ' . $endMonth . ' ' . $endDay;
-					if ( $format != 'noyears' ) $output .= ', ' . $endYear;
-					
-					
+					if ( $format != 'noyears' ) $output .= ', ' . $endYear;	
 				} else {
 					// Same Months
 					if ($startDay !== $endDay) {
@@ -181,84 +159,75 @@ function create_venue_tax() {
 					}
 				}
 			}
-			
-			
 			if ( $s_hour == '12' && $s_min == '00' && $s_am == ' am' && $e_hour == '11' && $e_min == '59') {
 				// Is All Day, no output	
 			} else {
 				// Not All Day
-				
 				// Set PM, and also output format
 				if ( $s_am == " am" ) :
 					$s_am = "am";
-					else :
-						$s_am = "pm";		
-					endif;
-					
-					if ( $e_am == " am" ) :
-						$e_am = "am";
-						else :
-							$e_am = "pm";		
-						endif;
-						
-						if ( $s_min == '00' ) {
-							if($s_hour == "12"){
-								if($s_am == "am"){
-									$output .= ", Midnight";
-								}else{
-									$output .= ", Noon";
-								}
-							}else{
-								if ( is_page_template('single-events.php'))	 {
-									$output .= '</p><p>' . $s_hour;									
-								} else {
-									$output .= ' @ ' . $s_hour;
-								}
-								if ( $s_am !== $e_am ) {
-									// Different AM
-									$output .= '' . $s_am;
-								}elseif ( $e_hour == "12" && $e_min == '00' ) {
-									// End time is midnight or noon
-									$output .= '' . $s_am;
-								}
-								
-							}
-						} else {
-							if ( is_page_template('single-events.php'))	 {
-								$output .= '</p><p>' . $s_hour . ':' . $s_min;
-							} else {
-								$output .= ' @ ' . $s_hour . ':' . $s_min;
-							}
-							if ( $s_am !== $e_am ) {
-								// Different AM
-								$output .= ' ' . $s_am;
-							}
+				else :
+					$s_am = "pm";		
+				endif;
+				if ( $e_am == " am" ) :
+					$e_am = "am";
+				else :
+					$e_am = "pm";
+				endif;
+				if ( $s_min == '00' ) {
+					if($s_hour == "12"){
+						if($s_am == "am"){
+							$output .= ", Midnight";
+						}else{
+							$output .= ", Noon";
 						}
-						
-						if ( $e_min == '00' ) {
-							if($e_hour == "12"){
-								if($e_am == "am"){
-									$output .= "&ndash;Midnight";
-								}else{
-									$output .= "&ndash;Noon";
-								}
-							}else{
-								$output .= '&ndash;' . $e_hour;
-								$output .= $e_am;
-							}
+					}else{
+						if ( is_singular() )	 {
+							$output .= '</p><p>' . $s_hour;
 						} else {
-							$output .= '&ndash;' . $e_hour . ':' . $e_min;
-							$output .= $e_am;
+							$output .= ' @ ' . $s_hour;
+						}
+						if ( $s_am !== $e_am ) {
+							// Different AM
+							$output .= '' . $s_am;
+						}elseif ( $e_hour == "12" && $e_min == '00' ) {
+							// End time is midnight or noon
+							$output .= '' . $s_am;
 						}
 						
 					}
-					
+				} else {
+					if ( is_singular() ) {
+						$output .= '</p><p>' . $s_hour . ':' . $s_min;
+					} else {
+						$output .= ' @ ' . $s_hour . ':' . $s_min;
+					}
+					if ( $s_am !== $e_am ) {
+						// Different AM
+						$output .= ' ' . $s_am;
+					}
 				}
-				
-				//if ( $recurrence_text != '' ) $output .= '<br />' . $recurrence_text;
-				
-				return $output;
-				
+				if ( $e_min == '00' ) {
+					if($e_hour == "12"){
+						if($e_am == "am"){
+							$output .= "&ndash;Midnight";
+						}else{
+							$output .= "&ndash;Noon";
+						}
+					}else{
+						$output .= '&ndash;' . $e_hour;
+						$output .= $e_am;
+					}
+				} else {
+					$output .= '&ndash;' . $e_hour . ':' . $e_min;
+					$output .= $e_am;
+				}
 			}
-			
-			?>
+		}
+		//if ( $recurrence_text != '' ) $output .= '<br />' . $recurrence_text;
+		return $output;
+	}
+}
+add_action( 'wp_enqueue_scripts', 'event_dates' );
+
+?>
