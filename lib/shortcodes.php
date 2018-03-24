009754888222ea272204feae99b3908c29d73b37
Nando Ramirez <erbnando@gmail.com>
2018-03-24 15:33:02 -0400

    updates to shortcodes

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,32 +1,9 @@
 <?php
 
-function slider_func() {
-	$output = '<div class="project-slider">';
-	foreach( get_field("project_image_slider") as $item ):
-		$output .= '<div class="project-slide">';
-		$output .= '<img src="' . $item['sizes']['slider'] . '" />';
-		if ($item['caption']) {
-			$output .= '<div class="caption caption-left">';
-			$output .= $item['caption'];
-			$output .= '</div>';
-		}
-		if ($item['description']) {
-			$output .= '<div class="caption caption-right">';
-			$output .= $item['description'];
-			$output .= '</div>';
-		}
-		$output .= '</div>';
-	endforeach;
-	$output .= '</div>';
-    
-    return $output;
-}
-add_shortcode( 'slider', 'slider_func' );
-
 function video_func( $atts ) {
     $a = shortcode_atts( array(
         'id' => '',
-		'source' => 'vimeo',
+		'source' => '',
 		'poster' => '',
     ), $atts );
 
