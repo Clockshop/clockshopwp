d0722fbd657fb71e36e983c366b2f1d028ccb63e
Clockshop Dev <erbnando+clockshop@gmail.com>
2018-04-08 22:38:58 -0400

    Updated event date and time

--- lib/events.php
+++ lib/events.php
@@ -144,10 +144,10 @@ function event_dates() {
 		$endTime = date('g', strtotime(get_field('_EventEndDate' , $postid)));
 		$s_hour = date('g', strtotime(get_field('_EventStartDate' , $postid)));
 		$s_min = date('i', strtotime(get_field('_EventStartDate' , $postid)));
-		$s_am = ' ' . date('a', strtotime(get_field('_EventStartDate' , $postid)));
+		$s_am = date('a', strtotime(get_field('_EventStartDate' , $postid)));
 		$e_hour = date('g', strtotime(get_field('_EventEndDate' , $postid)));
 		$e_min = date('i', strtotime(get_field('_EventEndDate' , $postid)));
-		$e_am = ' ' . date('a', strtotime(get_field('_EventEndDate' , $postid)));
+		$e_am = date('a', strtotime(get_field('_EventEndDate' , $postid)));
 		return spellerberg_show_event_datetime( $startYear, $endYear, $startMonth, $endMonth, $startDay, $endDay, $startTime, $endTime, $weekday, $s_hour, $s_min, $s_am, $e_hour, $e_min, $e_am, $format );	
 	}
 	function spellerberg_show_event_datetime ( $startYear, $endYear, $startMonth, $endMonth, $startDay, $endDay, $startTime, $endTime, $weekday, $s_hour, $s_min, $s_am, $e_hour, $e_min, $e_am, $format) {
@@ -201,102 +201,50 @@ function event_dates() {
 		echo '</p>';
 		*/
 
-		if ( $startYear == $endYear && $startMonth == "January" && $endMonth == "December" && $startDay == "1" && $endDay == "31") {
-			// Is a year-long program
-			$output .= 'Annual program';
+		if ( $startYear !== $endYear ) {
+			//Different Years
+			$output .= $startMonth . ' ' . $startDay . ', ' . $startYear;
+			$output .= ' &ndash; ' . $endMonth . ' ' . $endDay . ', ' . $endYear;
 		} else {
-			if ( $startYear !== $endYear ) {
-				//Different Years
-				$output .= $startMonth . ' ' . $startDay;
-				if ( $format != 'noyears' ) $output .= ', ' . $startYear;
-				$output .= ' &ndash; ' . $endMonth . ' ' . $endDay . ', ' . $endYear;
+			//Same Years
+			if ($startMonth !== $endMonth) {
+				// Different Months
+				$output .= $startMonth . ' ' . $startDay . ' &ndash; ' . $endMonth . ' ' . $endDay . ', ' . $endYear;	
 			} else {
-				//Same Years
-				if ($startMonth !== $endMonth) {
-					// Different Months
-					$output .= $startMonth . ' ' . $startDay . ' &ndash; ' . $endMonth . ' ' . $endDay;
-					if ( $format != 'noyears' ) $output .= ', ' . $endYear;	
+				// Same Months
+				if ($startDay !== $endDay) {
+					// Different Day
+					$output .= $startMonth . ' ' . $startDay . '&ndash;' . $endDay . ', ' . $endYear;
 				} else {
-					// Same Months
-					if ($startDay !== $endDay) {
-						// Different Day
-						$output .= $startMonth . ' ' . $startDay . '&ndash;' . $endDay;
-						if ( $format != 'noyears' ) $output .= ', ' . $endYear;
-					} else {
-						// Same Day
-						$output .= $startMonth . ' ' . $startDay;
-						if ( $format != 'noyears' ) $output .= ', ' . $startYear;
-					}
-				}
-			}
-			if ( $s_hour == '12' && $s_min == '00' && $s_am == ' am' && $e_hour == '11' && $e_min == '59') {
-				// Is All Day, no output
-			} elseif ($format == 'notime') {
-				// no output
-			} else {
-				// Not All Day
-				// Set PM, and also output format
-				if ( $s_am == " am" ) :
-					$s_am = "am";
-				else :
-					$s_am = "pm";		
-				endif;
-				if ( $e_am == " am" ) :
-					$e_am = "am";
-				else :
-					$e_am = "pm";
-				endif;
-				if ( $s_min == '00' ) {
-					if($s_hour == "12"){
-						if($s_am == "am"){
-							$output .= ", Midnight";
-						}else{
-							$output .= ", Noon";
-						}
-					}else{
-						if ( is_singular('events') ) {
-							$output .= '</p><p>' . $s_hour;
-						} else {
-							$output .= ' @ ' . $s_hour;
-						}
-						if ( $s_am !== $e_am ) {
-							// Different AM
-							$output .= '' . $s_am;
-						}elseif ( $e_hour == "12" && $e_min == '00' ) {
-							// End time is midnight or noon
-							$output .= '' . $s_am;
-						}
-						
-					}
-				} else {
+					// Same Day
+					$output .= $startMonth . ' ' . $startDay . ', ' . $startYear;
+					
+					if ( $s_am == "am" ) :
+						$s_am = "am";
+					else :
+						$s_am = "pm";		
+					endif;
+					if ( $e_am == "am" ) :
+						$e_am = "am";
+					else :
+						$e_am = "pm";
+					endif;
+			
 					if ( is_singular('events') ) {
 						$output .= '</p><p>' . $s_hour . ':' . $s_min;
 					} else {
-						$output .= ' @ ' . $s_hour . ':' . $s_min;
+						$output .= ', ' . $s_hour . ':' . $s_min;
 					}
 					if ( $s_am !== $e_am ) {
 						// Different AM
-						$output .= ' ' . $s_am;
+						$output .= '' . $s_am;
 					}
-				}
-				if ( $e_min == '00' ) {
-					if($e_hour == "12"){
-						if($e_am == "am"){
-							$output .= "&ndash;Midnight";
-						}else{
-							$output .= "&ndash;Noon";
-						}
-					}else{
-						$output .= '&ndash;' . $e_hour;
-						$output .= $e_am;
-					}
-				} else {
-					$output .= '&ndash;' . $e_hour . ':' . $e_min;
-					$output .= $e_am;
+			
+					$output .= '&ndash;' . $e_hour . ':' . $e_min . $e_am;
 				}
 			}
 		}
-		//if ( $recurrence_text != '' ) $output .= '<br />' . $recurrence_text;
+
 		return $output;
 	}
 }
