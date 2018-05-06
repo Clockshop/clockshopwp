4660b0f8e96fe0d93bf26e725ba56807c7445ea5
Nando Ramirez <erbnando@gmail.com>
2018-05-06 14:51:41 -0400

    removed unused secondary menu

--- lib/menus.php
+++ lib/menus.php
@@ -2,7 +2,6 @@
 
 function register_custom_menus() {
 	register_nav_menu('primary', __('Primary Menu'));
-	register_nav_menu('subnav', __('Secondary Menu'));
 }
 
 add_action('init', 'register_custom_menus');
